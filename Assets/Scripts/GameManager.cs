9b15f2e0b0a33f77f57e6ed1b08981687a2bcd84
Christian <CStochholm@kami-io.com>
2023-08-23 00:59:42 +0200

    Renamed spwaner to spawner, setup GM as singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,23 +7,57 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    // Creates an instance of Game Manager
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance is null)
+                Debug.LogError("Game Manager is NULL");
+
+            return _instance;
+        }
+    }
+
+    // GameState Variables
+    public GameState State;
+    public static event Action<GameState> OnGameStateChanged;
+
+
     [SerializeField]
     private Canvas _gameEnd;
     [SerializeField]
     private Timer time;
     [SerializeField]
-    private Spwaner _spwaner;
+    private Spawner spawner;
     [SerializeField]
     private Points points;
 
     static int _higScore;
 
-    void Update()
+    void Awake()
+    {
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+        void Update()
     {
         if(time.timeIsRunning == false)
         {
-            _spwaner.gameObject.SetActive(false);
+            spawner.gameObject.SetActive(false);
             _gameEnd.gameObject.SetActive(true);
         }
     }
+}
+
+public enum GameState
+{
+        MainMenu,
+        Start,
+        Playing,
+        Paused,
+        Won,
+        Lost,
 }
